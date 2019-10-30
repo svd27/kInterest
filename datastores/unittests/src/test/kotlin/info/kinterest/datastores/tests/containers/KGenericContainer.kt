a95ea219117d5399235284759df07b8c3b483758
svd <sdjuric68@gmail.com>
2019-10-30 02:51:08 +0100

    removed unittests

--- datastores/unittests/src/test/kotlin/info/kinterest/datastores/tests/containers/KGenericContainer.kt
+++
@@ -1,5 +0,0 @@
-package info.kinterest.datastores.tests.containers
-
-import org.testcontainers.containers.GenericContainer
-
-class KGenericContainer(image: String) : GenericContainer<KGenericContainer>(image)
